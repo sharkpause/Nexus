fdbc792a741bd865e80058620d000e79dc58c17b
sharkpause <carlmabfredishere@gmail.com>
2025-12-22 23:45:34 +0700

    feat: add a debug attribute to lexer error so printing is possible, also handled semicolon

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,8 +1,10 @@
 use crate::token::Token;
 
+#[derive(Debug)]
 pub enum LexerError {
+    GenericError,
     UnexpectedChar(char),
-    EndOfInput
+    EndOfInput,
 }
 
 pub struct Lexer {
@@ -33,29 +35,55 @@ impl Lexer {
 
     pub fn next_token(&mut self) -> Result<Token, LexerError> {
         self.skip_whitespace();
-        
+
         let current_char =
             self.peek_char(0).ok_or(LexerError::EndOfInput)?;
 
         let mut token = String::new();
+
         if current_char.is_digit(10) {
             while let Some(character) = self.peek_char(0) {
                 if character.is_digit(10) {
                     token.push(character);
                     self.consume_char();
                 } else {
+                    if character.is_alphabetic() {
+                        return Err(LexerError::UnexpectedChar(character));
+                    }
                     break;
                 }
             }
-        }
 
-        let number: i64 =
-            token.parse().map_err(
+            let number: i64 =
+                token.parse().map_err(
                 |_| LexerError::UnexpectedChar(
                     token.chars().last().unwrap_or(' ')
                 ))?;
+        
+            return Ok(Token::IntLiteral(number));
+        } else if current_char.is_alphanumeric() {
+            while let Some(character) = self.peek_char(0) {
+                if character.is_alphanumeric() {
+                    token.push(character);
+                    self.consume_char();
+                } else {
+                    break;
+                }
+            }
 
-        return Ok(Token::IntLiteral(number));
+            if token == "return" {
+                return Ok(Token::Return);
+            } else {
+                return Err(LexerError::GenericError);
+            }
+        } else if current_char == ';' {
+            token.push(current_char);
+            self.consume_char();
+
+            return Ok(Token::Semicolon);
+        } else {
+            return Err(LexerError::GenericError);
+        }
     }
 
     pub fn tokenize(&mut self) -> Result<Vec<Token>, LexerError> {
