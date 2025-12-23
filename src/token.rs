6eb1e4f9d09e85ba6e54b3fd2d796ab0b6c05df9
sharkpause <carlmabfredishere@gmail.com>
2025-12-23 13:33:41 +0700

    feat: made new tokens for functions

--- src/token.rs
+++ src/token.rs
@@ -1,19 +1,47 @@
 pub enum Token {
     Return,
     IntLiteral(i64),
-    Semicolon
+    Semicolon,
+    LeftBrace,
+    RightBrace,
+    LeftParentheses,
+    RightParentheses,
+    Function,
+    IntType,
+    Identifier(String)
 }
 
 pub fn print_token(token: &Token) {
     match token {
         Token::Return => {
-            println!("Return token");
+            println!("Return");
         },
         Token::IntLiteral(number) => {
             println!("integer {}", number);
         },
         Token::Semicolon => {
-            println!("Semicolon token");
+            println!("Semicolon");
+        },
+        Token::LeftBrace => {
+            println!("Left brace");
+        },
+        Token::RightBrace => {
+            println!("Right brace");
+        },
+        Token::LeftParentheses => {
+            println!("Left parentheses");
+        },
+        Token::RightParentheses => {
+            println!("Right parentheses");
+        },
+        Token::Function => {
+            println!("Function keyword");
+        },
+        Token::IntType => {
+            println!("Integer type");
+        }
+        Token::Identifier(string) => {
+            println!("Identifier {}", string);
         }
     }
 }
